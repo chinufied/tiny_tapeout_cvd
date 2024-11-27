18b37eff6f1626889ec034cc37185f237c000ba9
chinufied <132874843+chinufied@users.noreply.github.com>
2024-11-27 14:22:05 +0530

    Update shiftreg_20k.v

--- src/shiftreg_20k.v
+++ src/shiftreg_20k.v
@@ -38,7 +38,7 @@ module tt_um_shiftreg (
     input [7:0] data_in,
     output [7:0] data_out
     );
-    parameter N = 99;  // Number of registers
+    parameter N = 20;  // Number of registers
     reg [7:0] reg_array [0:N-1];
 
     integer i;
