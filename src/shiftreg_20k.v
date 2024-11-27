d3d44dde7614b89355275e1128b4ec0d41444c16
chinufied <132874843+chinufied@users.noreply.github.com>
2024-11-27 11:03:12 +0530

    Update shiftreg_20k.v change in uio_out=0 for lint pass

--- src/shiftreg_20k.v
+++ src/shiftreg_20k.v
@@ -18,11 +18,11 @@ module tt_um_shiftreg (
 
   // All output pins must be assigned. If not used, assign to 0.
   //assign uo_out  = 0;  // Example: ou_out is the sum of ui_in and uio_in
-  //assign uio_out = 0;
+  assign uio_out = 0;
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-    wire _unused = &{uio_out,uio_in, 1'b0};
+    wire _unused = &{uio_in, 1'b0};
    shiftreg sr(
     .clk(clk),
     .rst(rst_n),
