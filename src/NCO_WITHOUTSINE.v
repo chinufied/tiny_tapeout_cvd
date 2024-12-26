858c1e84a8cb0053b1099ee9aa4d4894c2c6cac7
chinufied <132874843+chinufied@users.noreply.github.com>
2024-12-26 15:38:39 +0530

    Update NCO_WITHOUTSINE.v
    update in wrapper

--- src/NCO_WITHOUTSINE.v
+++ src/NCO_WITHOUTSINE.v
@@ -1,14 +1,43 @@
-module DDS_top #(parameter width_top=12)(clock_in,enable_in,wavesel_in,FTW_IN,amp_in,finwave_out);  //
+`default_nettype none
+
+module tt_um_dds (
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
+
+  // All output pins must be assigned. If not used, assign to 0.
+  //assign uo_out  = 0;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uio_out = 0;
+  assign uio_oe  = 0;
+
+  // List all unused inputs to prevent warnings
+	wire _unused = &{rst_n, 1'b0};
+   DDS_top dds_nosine(
+    .clock_in(clk),
+    .enable_in(ena),
+    .wavesel_in(ui_in[7:6]),
+    .FTW_IN(uio_in[5:0]),   
+    .amp_in(ui_in[5:0]),
+    .finwave_out(uo_out[5:0])
+    );
+  endmodule
+module DDS_top #(parameter width_top=6)(clock_in,enable_in,wavesel_in,FTW_IN,amp_in,finwave_out);  //
  input clock_in , enable_in ;
  input [width_top-1:0] FTW_IN;
- input [11:0] amp_in;   // problem with amplitude
+	 input [5:0] amp_in;   // problem with amplitude
  input [1:0] wavesel_in;
  
- output signed [11:0] finwave_out;
+	 output signed [5:0] finwave_out;
  
- wire signed [12:0] amp_temp ; // to convert amp to signed 
- wire signed [11:0] nco_wave;
- wire signed [24:0] mult_result;   // xxxxx
+	 wire signed [6:0] amp_temp ; // to convert amp to signed 
+	 wire signed [5:0] nco_wave;
+	 wire signed [12:0] mult_result;   // xxxxx
  
  //wire signed [11:0] nco_wave_sign;
  assign amp_temp = {1'b0,amp_in};
@@ -22,24 +51,24 @@ NCO #(.width(width_top)) nco1
 
 
 assign mult_result = amp_temp * nco_wave;
-assign finwave_out = mult_result [23:12] ;      //    nco_wave
+	 assign finwave_out = mult_result [11:6] ;      //    nco_wave
 
 
 endmodule 
 
-module NCO #(parameter width = 12) (clk_in,enable_in,FTW_in,wavesel_in,wave_out);
+module NCO #(parameter width = 6) (clk_in,enable_in,FTW_in,wavesel_in,wave_out);
 
 input clk_in,enable_in;
 input [width-1 :0] FTW_in;
 input [1:0] wavesel_in;
 
-output reg signed [11:0] wave_out;
+	output reg signed [5:0] wave_out;
 
 //wire sine_en;
 wire updown_acc;
 wire [width-1 :0] phase_r;
-wire [11:0] sine1;
-wire signed [11:0] square1,sawtooth1,triangular1 ;
+	wire [5:0] sine1;
+	wire signed [5:0] square1,sawtooth1,triangular1 ;
 
 accumulator #(.W(width)) i0  
        (	  .clk_in(clk_in), .enable_in(enable_in),
@@ -61,13 +90,13 @@ end
 assign updown_acc = (wavesel_in == 2'b11) ? 1'b1 : 1'b0;
 //assign sine_en = (wavesel_in == 2'b00) ? 1'b1 : 1'b0;
 
-assign sawtooth1 = phase_r[width-1 : width-12] - 2048;
-assign triangular1 = phase_r[width-1 : width-12] - 2048; //truncating phase accumulator bits to 12 
-assign square1 = (phase_r[11]==1) ? 12'b100000000000 : 12'b011111111111;
+	assign sawtooth1 = phase_r[width-1 : width-6] - 2048;
+	assign triangular1 = phase_r[width-1 : width-6] - 2048; //truncating phase accumulator bits to 12 
+	assign square1 = (phase_r[5]==1) ? 12'b100000000000 : 12'b011111111111;
 
 endmodule 
 
-module accumulator #(parameter W = 12) (clk_in,enable_in,updown_in,freq_in,count_out);
+module accumulator #(parameter W = 6) (clk_in,enable_in,updown_in,freq_in,count_out);
 input clk_in,enable_in,updown_in;
 input [W-1 :0] freq_in;
 output [W-1 :0] count_out; 
